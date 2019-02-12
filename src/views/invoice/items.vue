06d9c0b1656b2b386157005a3f54eddce2cff500
Chris Toma <cristian.scribul@gmail.com>
2019-02-12 21:09:11 +0200

    First commit

---
+++ src/views/invoice/items.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "items"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/invoice/partner.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "partner"
+    }
+</script>
+
+<style scoped>
+
+</style>
